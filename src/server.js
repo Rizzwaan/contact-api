3a6ce7d9f4c886d65aba85f1b5653affc5eaef0b
Md Akram <rizwan.mountblue@embibe.com>
2019-11-14 23:06:15 +0530

    Adds custom error handler

--- src/server.js
+++ src/server.js
@@ -10,5 +10,19 @@ app.use(morgan('dev'));
 
 app.use('/api/contacts', contactRoutes)
 
+//handle error
+app.use((req,res,next) => {
+  const error = new Error('Not Found!')
+  error.status = 404
+  next(error)
+})
+
+//custom error handler
+app.use((error, req, res, next ) => {
+  res.status(error.status || 500);
+  res.json({
+    message : error.message
+  })
+})
 
 app.listen(PORT, () => console.log('server running on port 3000'));
