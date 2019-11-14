e6d05dd907a6cd384d3c51b36780acfea0c9bc19
Md Akram <rizwan.mountblue@embibe.com>
2019-11-14 23:12:23 +0530

    Solves the cors issue

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Clone the repo 
 
-checkout to part-1 branch 
+checkout to part-1 branch for part-1 
 
 git checkout part-1
 
 npm install
 
 node src/server.js
+
+checkout to part-2 branch for part-2 code 
+
--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ const contactRoutes = require('./routes/contacts')
 const PORT = 3000;
 const morgan = require('morgan');
 
+//solve the cors issue
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if( req.method == 'OPTIONS') {
+    res.header('Access-Control-Allow-Method', 'PUT, POST, PATCh, DELETE, GET');
+    res.status(200).json({})
+  }
+})
 
 // using as middleware
 app.use(morgan('dev'));
